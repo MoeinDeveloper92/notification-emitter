10a67fac1b0c4def80b5a2b3a346763625656a6b
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-07-17 15:16:58 +0200

    adding comment

--- server/config/accessTokenAzure.js
+++ server/config/accessTokenAzure.js
@@ -9,24 +9,24 @@ const config = {
 };
 
 //instead of bellow function we can make use of fetch Request
-const obtainToken = async () => {
-  const res = await fetch(`${auth.authority}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: {
-      grant_type: "client_credentials",
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      scope: "https://graph.microsoft.com/.default",
-    },
-  });
+// const obtainToken = async () => {
+//   const res = await fetch(`${auth.authority}`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: {
+//       grant_type: "client_credentials",
+//       client_id: process.env.CLIENT_ID,
+//       client_secret: process.env.CLIENT_SECRET,
+//       scope: "https://graph.microsoft.com/.default",
+//     },
+//   });
 
-  const data = res.json();
-  const { access_token } = data;
-  const myToken = access_token;
-};
+//   const data = res.json();
+//   const { access_token } = data;
+//   const myToken = access_token;
+// };
 
 const pca = new msal.PublicClientApplication(config);
 
