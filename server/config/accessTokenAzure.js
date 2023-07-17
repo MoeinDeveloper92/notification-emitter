1ce25f4e067c96f77e042c7c3c79ec9b5cba9f22
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-07-17 15:14:20 +0200

    some changes

--- server/config/accessTokenAzure.js
+++ server/config/accessTokenAzure.js
@@ -4,8 +4,28 @@ const config = {
   auth: {
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.SECRET_ID,
-    authority: `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
+    authority: `https://login.microsoftonline.com/${process.env.TENANT_ID}/oauth2/v2.0/token`,
   },
+};
+
+//instead of bellow function we can make use of fetch Request
+const obtainToken = async () => {
+  const res = await fetch(`${auth.authority}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: {
+      grant_type: "client_credentials",
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      scope: "https://graph.microsoft.com/.default",
+    },
+  });
+
+  const data = res.json();
+  const { access_token } = data;
+  const myToken = accessToken;
 };
 
 const pca = new msal.PublicClientApplication(config);
